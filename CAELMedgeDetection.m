b7e438c3013cb852cdb8c4f70751d6d986e312cf
yanyan-cas <yanyan.cas@gmail.com>
2017-11-14 18:54:09 +0000

    Reference configuration
    Reference configuration added.

--- CAELMedgeDetection.m
+++ CAELMedgeDetection.m
@@ -26,14 +26,31 @@ for i = 2 : m-1
                         Z(i, j-1),     Z(i, j),      Z(i, j+1); ...
                         Z(i+1, j-1), Z(i+1, j) ,  Z(i+1, j+1)];
         temp2 = reshape(temp', 9, 1); % pay attention to the reshape is column-wise arrangement!
-        X((i-1) * (j-1), :) = temp2';
+        X((i-2) * n + j-1, :) = temp2';
     end
 end
 
 %% Reference ending configuration and output data
- 
 
+temp = imbinarize(imageGray);
+edgeImage = edge(B, 'sobel');
+%edgeImage = edge(B, 'canny');
+%edgeImage = edge(B, 'Roberts');
 
+[m, n] = size(edgeImage);
+
+Y = false(m * n, 2); % the indicator vector
+
+for i = 1 : m
+    for j = 1 : n
+        % get the neighbor cells' states
+       if (edgeImage(i, j) == 0)
+           Y((i - 1) * 512 + j, :) = [0 1];
+       else
+            Y((i - 1) * 512 + j, :)= [1 0];
+       end
+    end
+end
 
 
 %% optimal transition function and edge purification
