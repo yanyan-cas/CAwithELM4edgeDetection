3a77148d516ed7f52e5240f9e56a7dca6e3405fb
yanyan-cas <yanyan.cas@gmail.com>
2017-11-24 16:05:57 +0000

    Image purication for elm outputs.
    Image purication for elm outputs.

--- CAELMedgeDetection.m
+++ CAELMedgeDetection.m
@@ -1,14 +1,15 @@
 
-
+clc
+clear all
 %% Initial configuration and input data
 fprintf('Loading image data file\n'); 
 addpath('/Users/Yanyan/Documents/MATLAB/DATASET/SIPI_Database/aerials');
 
-image = imread('2.1.09.tiff');
+image = imread('3.2.25.tiff');
 
-imageGray = rgb2gray(image);
-
-B = imageGray;
+%imageGray = rgb2gray(image);
+B= image;
+%B = imageGray;
 
 % bouTempRow = uint8(zeros(1, size(B, 2)));
 % bouTempColumn = uint8(zeros(size(B, 1) + 2, 1));
@@ -35,134 +36,119 @@ X = stateMatrixGen(B);
 %temp = imbinarize(imageGray);
 figure;
 imshow(B);
-edgeImage = edge(B, 'sobel');
-figure;
-imshow(edgeImage);
-%edgeImage = edge(B, 'canny');
-%edgeImage = edge(B, 'Roberts');
+edgeImage1 = edge(B, 'sobel');
 
-[m, n] = size(edgeImage);
+edgeImage2 = edge(B, 'canny');
 
-Y = false(m * n, 2); % the indicator vector
+edgeImage3 = edge(B, 'Roberts');
 
-for i = 1 : m
-    for j = 1 : n
-        % get the neighbor cells' states
-       if (edgeImage(i, j) == 0)
-           Y((i - 1) * 512 + j, :) = [0 1];
-       else
-            Y((i - 1) * 512 + j, :)= [1 0];
-       end
-    end
-end
+%generate the indicator vector (elm output)
+Y1 = generateOutputfromImage(edgeImage1);
+Y2 = generateOutputfromImage(edgeImage2);
+Y3 = generateOutputfromImage(edgeImage3);
+Y = Y1 | Y2 | Y3;
 
-
+edgeImage = edgeImage1 | edgeImage2 | edgeImage3;
 %% optimal transition function and edge purification
 
-numHiddenNode = 100;
+errorRate = 1;
+elmRound = 10;
+
+%*********Initialization***********
+numHiddenNode = 500; 
 numInputNode = size(X, 2);
 numTrainData= size(X, 1);
-train_data = double(X');
-%train_data = mapminmax(double(X'), -1 , 1);
 
 
 
-%startTime = cputime;
 
 inputWeight = rand(numHiddenNode, numInputNode)*2 - 1;
 biasHiddenNeurons=rand(numHiddenNode,1);
-
-tempH=inputWeight * train_data;
-
 ind=ones(1,numTrainData);
-%   Extend the bias matrix BiasofHiddenNeurons to match the demention of H
-biasMatrix=biasHiddenNeurons(:,ind);            
-tempH=tempH+biasMatrix;
+biasMatrix=biasHiddenNeurons(:,ind);          
 
-activationFunction = 'sig';
-%%%%%%%%%%% Calculate hidden neuron output matrix H
-switch lower(activationFunction)
-    case {'sig','sigmoid'}
-        %%%%%%%% Sigmoid 
-        H = 1 ./ (1 + exp(-tempH));
-    case {'sin','sine'}
-        %%%%%%%% Sine
-        H = sin(tempH);    
-    case {'hardlim'}
-        %%%%%%%% Hard Limit
-        H = hardlim(tempH);            
-        %%%%%%%% More activation functions can be added here                
-end
 
-temp = pinv(H');
-outputWeight = temp * double(Y);
-
-outputY=logical(round(H' * outputWeight));     
-
-right = 0;
-for i = 1 : size(Y, 1)
-    if isequal(Y(i,:), outputY(i, :))
-        right = right +1;
-    else
-        right = right;
-    end
-end
-
-accuracy = right / size(Y, 1);
-
-%% 
-[m, n] = size(edgeImage);
-
-%input = mapminmax(double(X), -1 , 1);
-T = 3;
-temp = zeros(numHiddenNode, 1);
-temp2 = zeros(numHiddenNode, 1);
-temp3 = zeros(numHiddenNode, 1);
-temp4 = zeros(numHiddenNode, 1);
-nextState = zeros(m * n, 1);
-edgeTemp = false(m * n, 1);
-
-I_ref = edgeImage;
-
-for t = 1 : T
-    if t == 1
-        input = X;
-    else
-        input = stateMatrixGen(uint8(reshape(nextState, m, n)'));
-    end
-for i = 1 : m * n
-    for j = 1 : numHiddenNode
-       temp(j, 1) = inputWeight(j, :) * input(i, :)' + biasHiddenNeurons(j,:);
-       temp2(j, 1) = 1 ./ (1 + exp(-temp(j, 1)));
-       temp3(j, 1) = temp2(j, 1) * outputWeight(j, 1);
-       temp4(j, 1) = temp2(j, 1) * outputWeight(j, 2);
-    end
-        nextState(i, 1) = fix(sum(temp3) / sum(temp4)) * 255;
-        if nextState(i, 1) >= 255
-            nextState(i, 1) = 255;
-            edgeTemp(i, 1) = 1;
-        else
-            nextState(i, 1) = 0;    
-            edgeTemp(i, 1) = 0;
+iteration = 0;
+while (errorRate < 0.001) || (iteration < 10)
+        iteration = iteration +1;
+        train_data = double(X');
+        tempH=inputWeight * train_data;
+        tempH=tempH+biasMatrix;
+        
+        activationFunction = 'sig';
+        %%%%%%%%%%% Calculate hidden neuron output matrix H
+        switch lower(activationFunction)
+            case {'sig','sigmoid'}
+                %%%%%%%% Sigmoid 
+                H = 1 ./ (1 + exp(-tempH));
+            case {'sin','sine'}
+                %%%%%%%% Sine
+                H = sin(tempH);    
+            case {'hardlim'}
+                %%%%%%%% Hard Limit
+                H = hardlim(tempH);            
+                %%%%%%%% More activation functions can be added here                
         end
+        
+        outputWeight =  pinv(H') * double(Y);
+        tempY= H' * outputWeight - 0.5;     
+        outputY = tempY>=0;
+        %errorRate evaluation
+        k = 0;
+        for i = 1 : size(Y, 1)
+            if isequal(Y(i,:), logical(outputY(i, :)))
+                k = k +1;
+            end
+        end
+        errorRate = 1 - k / size(Y, 1);
+
+        
 end
 
-    figure;
-    I_edge = reshape(edgeTemp, m, n)';
-    imshow(I_edge);
-    
-    I_puri = imReplace(I_edge, I_ref);
-    figure;
-    imshow(I_puri);
-    
-end
-    
+displayInfo = sprintf("ELM error rate is %2f, training round = %d\n", errorRate, iteration );
+fprintf(displayInfo);
 
-for round = 1 : T
-    
-    
-end
+[Iedge1, I_puri1] = imagePurication(X, edgeImage, inputWeight, numHiddenNode, biasHiddenNeurons, outputWeight);
 
 %%
+%%%%%%%%%%test below
+Y = generateOutputfromImage(Iedge1);
+errorRate = 1;
+while errorRate > 0.05
+        train_data = double(X');
+        tempH=inputWeight * train_data;
+        tempH=tempH+biasMatrix;
+        
+        activationFunction = 'sig';
+        %%%%%%%%%%% Calculate hidden neuron output matrix H
+        switch lower(activationFunction)
+            case {'sig','sigmoid'}
+                %%%%%%%% Sigmoid 
+                H = 1 ./ (1 + exp(-tempH));
+            case {'sin','sine'}
+                %%%%%%%% Sine
+                H = sin(tempH);    
+            case {'hardlim'}
+                %%%%%%%% Hard Limit
+                H = hardlim(tempH);            
+                %%%%%%%% More activation functions can be added here                
+        end
+        
+        outputWeight =  pinv(H') * double(Y);
+        tempY= H' * outputWeight - 0.5;     
+        outputY = tempY>=0;
+        %errorRate evaluation
+        k = 0;
+        for i = 1 : size(Y, 1)
+            if isequal(Y(i,:), logical(outputY(i, :)))
+                k = k +1;
+            end
+        end
+        errorRate = 1 - k / size(Y, 1);
+
+        
+end
+
+[I_edge2, I_puri2] = imagePurication(X, edgeImage, inputWeight, numHiddenNode, biasHiddenNeurons, outputWeight);
 
 
